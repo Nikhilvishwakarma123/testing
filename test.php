bd4a3edd197d42fc3aa6f34baff1498ee1029e3d
Nikhilvishwakarma123 <Nikhilvishwakarma123@gmail.com>
2022-12-24 17:32:59 +0530

    intial upload

--- test.php
+++ test.php
@@ -2,17 +2,17 @@
 $array = ['red', 'green', 'blue', 'yellow', 'gray', 'black', 'orange'];
 
 // foreach ($array as $key => $value) {
-//     echo "<p style='background: $value'>" . $key . "</P>";
+//     echo "<p style='background: $value'>" . $value . "</P>";
 // }
 
 foreach ($array as $key => $value) {
     if ($value == "red") {
         echo "<p style='color:red'> Red </p>";
     } elseif ($value == "green") {
-        echo "<p style='color:green'>Green</p>";
+        echo "<p style='color:green'> Green </p>";
     } elseif ($value == "blue") {
-        echo "<p style='color:blue'>Blue</p>";
+        echo "<p style='color:blue'> Blue </p>";
     } elseif ($value == "orange") {
-        echo "<p style='color:orange'>Orange</p>";
+        echo "<p style='color:orange'> Orange </p>";
     }
 }
